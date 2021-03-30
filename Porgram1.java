74661781dbd5337ba94d058aa0b8c4acd921fe15
Simran321 <66427058+Simran321@users.noreply.github.com>
2021-03-30 22:17:35 +0530

    Add files via upload
    Display an int , double or char on scrren

---
+++ Porgram1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Porgram1 {
+    public static void main(String args[])
+    {
+        int a =20;
+        double b =20.655;
+        char c ='A';
+        System.out.println("int value : "+a);
+        System.out.println("double value : "+b);
+        System.out.println("char value : "+c);
+    }
+}
