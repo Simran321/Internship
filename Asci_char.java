9cab4abc522c79c9dd299cef092f78bd2714cd11
Simran321 <66427058+Simran321@users.noreply.github.com>
2021-03-30 22:47:54 +0530

    Add files via upload
    Display ascii value of h

---
+++ Asci_char.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Asci_char {
+    public static void main(String args[])
+    {
+        int i='h';
+        System.out.println(i);
+    }
+}
