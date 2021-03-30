772a1d3d1c46973b95293ea076031e8f2516fdb4
Simran321 <66427058+Simran321@users.noreply.github.com>
2021-03-30 22:47:17 +0530

    Add files via upload
    Produt of 3 nos

---
+++ product.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class product {
+    public static void main(String args[])
+    {
+        int a=8;
+        int b=2;
+        int c=6;
+        System.out.println("Product="+(a*b*c));
+    }
+}
