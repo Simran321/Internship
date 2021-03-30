8e5297d26f73db004914363501ec6101e0814204
Simran321 <66427058+Simran321@users.noreply.github.com>
2021-03-30 22:49:04 +0530

    Add files via upload
    adding 3 to char d

---
+++ Add_to_Ascii.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Add_to_Ascii {
+    public static void main(String args[])
+    {
+        int i='d'+3;
+        char ch=(char) i;
+        System.out.println(ch);
+    }
+}
