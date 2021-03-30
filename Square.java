f444a6cd1c8d1af8251b5945e4016416c9c85d2b
Simran321 <66427058+Simran321@users.noreply.github.com>
2021-03-30 22:50:06 +0530

    Add files via upload
    calculating square

---
+++ Square.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Square {
+    public static void main(String args[])
+    {
+        double a=3.9;
+        double sq=Math.pow(a,2);
+        System.out.println(sq);
+    }
+}
