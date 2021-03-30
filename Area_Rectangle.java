341fe2d803eae274d0c78c0fd014176bd0e35f49
Simran321 <66427058+Simran321@users.noreply.github.com>
2021-03-30 22:23:58 +0530

    Add files via upload
    calculating area of rectangle

---
+++ Area_Rectangle.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Area_Rectangle {
+    public static void main(String args[])
+    {
+       int a =2;
+       int b =3;
+       System.out.println("Length of rectangle: "+a+" units");
+       System.out.println("Width of rectangle: "+b+" units");
+       System.out.println("Area of rectangle: "+(a*b)+" units");
+       
+    }
+}
